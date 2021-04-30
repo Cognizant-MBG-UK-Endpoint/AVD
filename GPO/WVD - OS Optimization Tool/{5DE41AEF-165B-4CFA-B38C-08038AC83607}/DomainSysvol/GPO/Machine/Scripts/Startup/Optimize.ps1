1187457fd2851975d6892325e9646700ea3cfb26
lmurray-mbg <83401697+lmurray-mbg@users.noreply.github.com>
2021-04-30 15:38:31 +0100

    Add files via upload

---
+++ GPO/WVD - OS Optimization Tool/{5DE41AEF-165B-4CFA-B38C-08038AC83607}/DomainSysvol/GPO/Machine/Scripts/Startup/Optimize.ps1
@@ -0,0 +1,34 @@
+﻿$softwareRepo = "C:\Scripts"
+$WantFile = "C:\Scripts\Virtual-Desktop-Optimization-Tool-main\Optimized.txt"
+$FileExists = Test-Path $WantFile
+
+# Run Windows 10 multi-session optimization script
+If ($FileExists -eq $True) {exit}
+Else {
+# Create repository for WVD OS optimization tool
+Write-Host "Creating repostiory for WVD OS optimization tool"
+New-Item -ItemType Directory -Path $softwareRepo -Force -ErrorAction SilentlyContinue
+
+# Download WVD OS optimization tool
+Write-Host "Downloading WVD OS optimization tool"
+Invoke-WebRequest -Uri "https://github.com/Cognizant-MBG-UK-Endpoint/WVD/blob/main/Optimize/Virtual-Desktop-Optimization-Tool-main.zip?raw=true" -OutFile "$softwareRepo\Optimize.zip"
+
+# Extract WVD OS optimization tool
+Write-Host "Extracting WVD OS optimization tool"
+Expand-Archive -Path "$softwareRepo\Optimize.zip" -DestinationPath "$softwareRepo" -Force
+Remove-Item -Path "$softwareRepo\Optimize.zip" -Force
+
+# Execute WVD OS optimization tool
+Write-Host "Executing WVD OS optimization tool"
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
+Start-Process -FilePath PowerShell.exe -ArgumentList "$softwareRepo\Virtual-Desktop-Optimization-Tool-main\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2009 -Verbose" -Wait
+Write-Host "Completed WVD OS optimization"
+
+# Create verification check text file
+Write-Host "Creating verification check text file"
+New-Item -ItemType File -Path "$softwareRepo\Virtual-Desktop-Optimization-Tool-main\Optimized.txt" -Force
+
+# Restart computer
+Write-Host "Restarting computer"
+Restart-Computer -Force
+}
---
+++ GPO/WVD - Region & Language/{70064C01-6B46-43C8-8715-5CDCDE2C2FF6}/DomainSysvol/GPO/User/Scripts/Logon/Region_Language_Time.ps1
@@ -0,0 +1,5 @@
+﻿Set-WinSystemLocale en-GB
+Set-WinUserLanguageList -LanguageList en-GB -Force
+Set-Culture -CultureInfo en-GB
+Set-WinHomeLocation -GeoId 242
+Set-TimeZone "GMT Standard Time"
